c2db19fc5a146c256a8f97512a997ca1de2ae808
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-07 19:02:03 +0000

    Wed, Aug 7, 2024, 7:02 PM +00:00

--- server/index.js
+++ server/index.js
@@ -1,14 +1,3 @@
-# WebSockets
-
-## Agenda 1: WebSocket establishes a handshake between server and client
-
-```sh
-yarn add uuid websocket
-```
-
-`server/index.js`
-
-```js
 const { WebSocketServer } = require("ws");
 const http = require("http");
 
@@ -19,4 +8,3 @@ const port = 8000;
 server.listen(port, () => {
   console.log(`WebSocket server is running on port ${port}`);
 });
-```
