4f82d2dbf9df245a16b49af275732deda6ced4a4
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-07 19:06:14 +0000

    Wed, Aug 7, 2024, 7:06 PM +00:00

--- server/index.js
+++ server/index.js
@@ -8,3 +8,16 @@ const port = 8000;
 server.listen(port, () => {
   console.log(`WebSocket server is running on port ${port}`);
 });
+// I'm maintaining all active connections in this object
+const clients = {};
+
+// A new client connection request received
+wsServer.on("connection", function (connection) {
+  // Generate a unique code for every user
+  const userId = uuidv4();
+  console.log(`Recieved a new connection.`);
+
+  // Store the new connection and handle messages
+  clients[userId] = connection;
+  console.log(`${userId} connected.`);
+});
